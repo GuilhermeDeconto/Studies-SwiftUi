0201fb184dd3134b0aab245848aa75a45761e154
GuilhermeDeconto <deconto100@gmail.com>
2020-01-07 14:03:27 -0300

    Starting project

---
+++ SwiftUi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUi
+//
+//  Created by Guilherme Deconto on 07/01/20.
+//  Copyright © 2020 Guilherme Deconto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
